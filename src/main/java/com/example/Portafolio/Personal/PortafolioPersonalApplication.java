0dc2796e46a89ca0a864df53c6959d8d1975b9a4
Benjamin <benjamin.barcelo267309@potros.itson.edu.mx>
2026-09-16 23:14:04 -0700

    Initial commit

---
+++ src/main/java/com/example/Portafolio/Personal/PortafolioPersonalApplication.java
@@ -0,0 +1,14 @@
+package com.example.Portafolio.Personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioPersonalApplication.class, args);
+	}
+
+	//            http://localhost:8080/
+}
